df26237d757324f4207b2bc8c40f17875006036e
zach111694 <zach111694@gmail.com>
2016-02-08 14:59:37 -0500

    Added questions json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+	fs.readFile("../data/questions.json", "utf8", function read(error,text) {
+		if (error){
+			throw error;
+		}
+
+    	var questionsData = JSON.parse(text);
+    	res.render('index', {title: 'Mentoring', questionsData: questionsData});
+	});
+	
 });
 
 module.exports = router;
