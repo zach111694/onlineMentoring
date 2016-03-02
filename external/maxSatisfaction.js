c1612e32e55964fe2d8b8df454c981e985224db9
galefeilian <137113610@qq.com>
2016-03-02 14:36:12 -0500

    fixed names and parameter passing problems
    "mens" are replaced by "mentors"
    "ss" are replaced by " mentees"
    "men" are replaced by ""mentor"
    "s" are replaced by "mentee"

--- external/maxSatisfaction.js
+++ external/maxSatisfaction.js
@@ -1,114 +1,112 @@
 var MaxAlgorithm = module.exports = {};
 
-MaxAlgorithm.maxFunction = function(mens,ss){
-	d={};
-    var a = [];
-    var b= [];
-    for (var i=0;i<mens.length;i++){
-        a.push(mens[i]);
-    };
-    for(var j=0;j<ss.length;j++){
-        b.push(ss[j]);
-    };
-	function maxSatisfaction(mens,ss){
-    	var maxScoreSoFar = 0;
-    	if (mens.length === 0|| ss.length === 0 ){
-    			if (! ([mens] in d)){
-                    d[mens] = {};
+MaxAlgorithm.maxFunction = function(mentors,mentees){
+    d={};
+    function maxSatisfaction(mentors,mentees){
+        var maxScoreSoFar = 0;
+        if (mentors.length === 0|| mentees.length === 0 ){
+                if (! (mentors in d)){
+                    d[mentors] = {};
                 }
-    			d[mens][ss]={};
-    			d[mens][ss].score=0;
-    			d[mens][ss].choices=[];
-    	}else{
-        	for (var men = 0; men < mens.length; men++){
-    				for (var s = 0; s < ss.length; s++){var copyOfMensAfterSelection = mens.slice(0,men);
-    					copyOfMensAfterSelection = copyOfMensAfterSelection.concat(mens.slice(men+1,mens.length));
-    					var copyOfSsAfterSelection = ss.slice(0,s);
-    					copyOfSsAfterSelection = copyOfSsAfterSelection.concat(ss.slice(s+1,ss.length));
-    					if (!(([copyOfMensAfterSelection][copyOfSsAfterSelection]) in d)) {
-    						maxSatisfaction(copyOfMensAfterSelection,copyOfSsAfterSelection,d);
-    					}
-    					var score = calcScore(mens[men],ss[s]) + d[copyOfMensAfterSelection][copyOfSsAfterSelection].score;
-    					if (score > maxScoreSoFar){
-    					    maxScoreSoFar = score;
-    					    if (! ([mens] in d)){
-    					        d[mens] = {};
-    					    }
-    					    d[mens][ss]={};
-    						d[mens][ss].score = maxScoreSoFar;
-    						var choice = {};
-    						choice.men = mens[men];
-    						choice.s = ss[s];
-    						choiceOfNextLvl = d[copyOfMensAfterSelection][copyOfSsAfterSelection].choices;
-    						choiceOfNextLvl.push(choice);
-    						d[mens][ss].choices = choiceOfNextLvl;
-    					}
-    				}
-    	    }
-	    }
-	}
-	maxSatisfaction(a,b);
-	console.log(d[a][b].choices);
-}
-
-/*test code
-function getScore(m,s){
-	if(m === "a"){
-		if(s ==="d"){
-			return 3;
-		}else if(s==="e"){
-			return 5;
-		}else{return 4;}
-	}else if(m ==="b"){
-		if(s ==="d"){
-			return 2;
-		}else if(s==="e"){
-			return 4;
-		}else{return 6;}
-	}else{if(s ==="d"){
-			return 1;
-		}else if(s==="e"){
-			return 2;
-		}else{return 3;}
-	}
-}
-d={};
-function maxSatisfaction(mens,ss,d){
-    var maxScoreSoFar = 0;
-    if (mens.length === 0|| ss.length === 0 ){
-    		d[mens]={};
-    		d[mens][ss]={};
-    		d[mens][ss].score=0;
-    		d[mens][ss].choices=[];
-    }else{
-        for (var men = 0; men < mens.length; men++){
-    			for (var s = 0; s < ss.length; s++){var copyOfMensAfterSelection = mens.slice(0,men);
-    				copyOfMensAfterSelection = copyOfMensAfterSelection.concat(mens.slice(men+1,mens.length));
-    				var copyOfSsAfterSelection = ss.slice(0,s);
-    				copyOfSsAfterSelection = copyOfSsAfterSelection.concat(ss.slice(s+1,ss.length));
-    				if (!(([copyOfMensAfterSelection][copyOfSsAfterSelection]) in d)) {
-    					maxSatisfaction(copyOfMensAfterSelection,copyOfSsAfterSelection,d);
-    				}
-    				var score = getScore(mens[men],ss[s]) + d[copyOfMensAfterSelection][copyOfSsAfterSelection].score;
-    				if (score > maxScoreSoFar){
-    				    maxScoreSoFar = score;
-    				    if (! ([mens] in d)){
-    				        d[mens] = {};
-    				    }
-    				    d[mens][ss]={};
-    					d[mens][ss].score = maxScoreSoFar;
-    					var choice = new Object();
-    					choice.men = mens[men];
-    					choice.s = ss[s];
-    					choiceOfNextLvl = d[copyOfMensAfterSelection][copyOfSsAfterSelection].choices;
-    					choiceOfNextLvl.push(choice);
-    					d[mens][ss].choices = choiceOfNextLvl;
-    				}
-    			}
+                d[mentors][mentees]={};
+                d[mentors][mentees].score=0;
+                d[mentors][mentees].choices=[];
+        }else{
+            for (var mentor = 0; mentor < mentors.length; mentor++){
+                for (var mentee = 0; mentee < mentees.length; mentee++){var copyOfMentorsAfterSelection = mentors.slice(0,mentor);
+                    copyOfMentorsAfterSelection = copyOfMentorsAfterSelection.concat(mentors.slice(mentor+1,mentors.length));
+                    var copyOfMenteesAfterSelection = mentees.slice(0,mentee);
+                    copyOfMenteesAfterSelection = copyOfMenteesAfterSelection.concat(mentees.slice(mentee+1,mentees.length));
+                    if (!(([copyOfMentorsAfterSelection][copyOfMenteesAfterSelection]) in d)) {
+                        maxSatisfaction(copyOfMentorsAfterSelection,copyOfMenteesAfterSelection,d);
+                    }
+                    var score = calcScore(mentors[mentor],mentees[mentee]) + d[copyOfMentorsAfterSelection][copyOfMenteesAfterSelection].score;
+                    if (score > maxScoreSoFar){
+                        maxScoreSoFar = score;
+                        if (! (mentors in d)){
+                            d[mentors] = {};
+                        }
+                        d[mentors][mentees]={};
+                        d[mentors][mentees].score = maxScoreSoFar;
+                        var choice = {};
+                        choice.mentor = mentors[mentor];
+                        choice.mentee = mentees[mentee];
+                        choiceOfNextLvl = d[copyOfMentorsAfterSelection][copyOfMenteesAfterSelection].choices;
+                        choiceOfNextLvl.push(choice);
+                        d[mentors][mentees].choices = choiceOfNextLvl;
+                    }
+                }
+            }
         }
     }
+    maxSatisfaction(mentors,mentees);
+    console.log(d[mentors][mentees].choices);
+};
+
+/*test code
+function calcScore(m,s){
+    if(m === "a"){
+        if(s ==="d"){
+            return 3;
+        }else if(s==="e"){
+            return 5;
+        }else{return 4;}
+    }else if(m ==="b"){
+        if(s ==="d"){
+            return 2;
+        }else if(s==="e"){
+            return 4;
+        }else{return 6;}
+    }else{if(s ==="d"){
+            return 1;
+        }else if(s==="e"){
+            return 2;
+        }else{return 3;}
+    }
 }
-maxSatisfaction(["a","b","c"],["d","e","f"],d);
-console.log(d)
-console.log(d["a,b,c"]["d,e,f"])
+d={};
+function s(mentors,mentees){
+    d={};
+    function maxSatisfaction(mentors,mentees){
+        var maxScoreSoFar = 0;
+        if (mentors.length === 0|| mentees.length === 0 ){
+                if (! (mentors in d)){
+                    d[mentors] = {};
+                }
+                d[mentors][mentees]={};
+                d[mentors][mentees].score=0;
+                d[mentors][mentees].choices=[];
+        }else{
+            for (var mentor = 0; mentor < mentors.length; mentor++){
+                for (var mentee = 0; mentee < mentees.length; mentee++){var copyOfMentorsAfterSelection = mentors.slice(0,mentor);
+                    copyOfMentorsAfterSelection = copyOfMentorsAfterSelection.concat(mentors.slice(mentor+1,mentors.length));
+                    var copyOfMenteesAfterSelection = mentees.slice(0,mentee);
+                    copyOfMenteesAfterSelection = copyOfMenteesAfterSelection.concat(mentees.slice(mentee+1,mentees.length));
+                    if (!(([copyOfMentorsAfterSelection][copyOfMenteesAfterSelection]) in d)) {
+                        maxSatisfaction(copyOfMentorsAfterSelection,copyOfMenteesAfterSelection,d);
+                    }
+                    var score = calcScore(mentors[mentor],mentees[mentee]) + d[copyOfMentorsAfterSelection][copyOfMenteesAfterSelection].score;
+                    if (score > maxScoreSoFar){
+                        maxScoreSoFar = score;
+                        if (! (mentors in d)){
+                            d[mentors] = {};
+                        }
+                        d[mentors][mentees]={};
+                        d[mentors][mentees].score = maxScoreSoFar;
+                        var choice = {};
+                        choice.mentor = mentors[mentor];
+                        choice.mentee = mentees[mentee];
+                        choiceOfNextLvl = d[copyOfMentorsAfterSelection][copyOfMenteesAfterSelection].choices;
+                        choiceOfNextLvl.push(choice);
+                        d[mentors][mentees].choices = choiceOfNextLvl;
+                    }
+                }
+            }
+        }
+    }
+    maxSatisfaction(mentors,mentees);
+    console.log(d[mentors][mentees].choices);
+    console.log(d)
+};
+s(["a","b","c"],["d","e","f"]);
 */
