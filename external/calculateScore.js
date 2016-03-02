1db3c2019992c0414c6374de0ca0fee1b445c4f7
zach111694 <zach111694@gmail.com>
2016-03-02 17:48:37 -0500

    Modified calculateScore.js

--- external/calculateScore.js
+++ external/calculateScore.js
@@ -1,44 +1,29 @@
-//calculate compatibility score
-"use strict";
+
+// TEST OBJECT CREATED FROM CREATEOBJECT.JS (CARL'S FILE/FUNCTION)
+
+var mentors = { zyuzon: { q1: 'CSC', q2: 'Boxing', q3: 'Hearthstone', q4: 'Thai' },
+  cosias: { q1: 'MAT', q2: 'Basketball', q3: 'Skyrim', q4: 'Japanese' },
+  jschanker: { q1: 'CSC', q2: 'Baseball', q3: 'WoW', q4: 'Italian' },
+  testUser: { q1: 'MAT', q2: 'Football', q3: 'Skyrim', q4: 'Chinese' } };
+
+var mentees = { hstarkie: { q1: 'BUS', q2: 'Football', q3: 'WoW', q4: 'Italian' },
+  rhuang: { q1: 'MUS', q2: 'Baseball', q3: 'LoL', q4: 'Chinese' },
+  ramato: { q1: 'MAT', q2: 'Baseball', q3: 'LoL', q4: 'Japanese' },
+  mschultz: { q1: 'MAT', q2: 'Football', q3: 'Hearthstone', q4: 'Japanese' } };
 
 
-//testing
-
-var questionsObj = {
-	Q1: {val:2},
-	Q2: {val:3}
-};
-
-var men = {
-	Q1: "A",
-	Q2: "B"
-};
-
-var stu = {
-    Q1: "A",
-    Q2: "B"
-};
-
-/**
- * Returns compatibility score of mentor and mentee
- *@param {Object} mentor
- *@param {Object} student
- *@param {Object} questions
- *@returns {number}
-**/
-
-
-function calcScore(mentor,student,questions){
+function calcScore(mentor,mentee){
 	var score = 0;
-	for(var question in questions){
-	    console.log(mentor[question]);
-	    console.log(student[question]);
-		if(mentor[question] === student[question]){
-			var value = questions[question].val;
-			score += value;
+	mentorAnswers = mentors[mentor];
+	menteeAnswers = mentees[mentee];
+
+	for(var i in mentorAnswers){
+		if(mentorAnswers[i] == menteeAnswers[i]){
+			score += 1;
 		}
 	}
 	return score;
-}
+};
 
-calcScore(men,stu,questionsObj);
+
+console.log(calcScore('cosias','ramato'));
