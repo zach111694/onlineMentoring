0cca8b1e643e97fc7c85cd64f62b1364e279e321
zach111694 <zach111694@gmail.com>
2016-03-02 20:52:09 -0500

    Added test objects onto max satisfaction and updated database

--- external/maxSatisfaction.js
+++ external/maxSatisfaction.js
@@ -1,4 +1,13 @@
 var MaxAlgorithm = module.exports = {};
+var mentors = { zyuzon: { q1: 'CSC', q2: 'Boxing', q3: 'Hearthstone', q4: 'Thai' },
+  cosias: { q1: 'MAT', q2: 'Basketball', q3: 'Skyrim', q4: 'Japanese' },
+  jschanker: { q1: 'CSC', q2: 'Baseball', q3: 'WoW', q4: 'Italian' },
+  testUser: { q1: 'MAT', q2: 'Football', q3: 'Skyrim', q4: 'Chinese' } };
+
+var mentees = { hstarkie: { q1: 'BUS', q2: 'Football', q3: 'WoW', q4: 'Italian' },
+  rhuang: { q1: 'MUS', q2: 'Baseball', q3: 'LoL', q4: 'Chinese' },
+  ramato: { q1: 'MAT', q2: 'Baseball', q3: 'LoL', q4: 'Japanese' },
+  mschultz: { q1: 'MAT', q2: 'Football', q3: 'Hearthstone', q4: 'Japanese' } };
 
 MaxAlgorithm.maxFunction = function(mentorsObj,menteesObj){
     d={};
@@ -53,6 +62,8 @@ MaxAlgorithm.maxFunction = function(mentorsObj,menteesObj){
     maxSatisfaction(mentors,mentees);
     console.log(d[mentors][mentees].choices);
 };
+
+MaxAlgorithm.maxFunction(mentors,mentees);
 
 /*test code
 function s(mentorsObj,menteesObj){
